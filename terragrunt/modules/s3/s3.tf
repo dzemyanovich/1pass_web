5537091a11d502ec084f9158c32bc348953bc34d
Aliaksei Dzemyanovich <aliaksei.dzemyanovich@gmail.com>
2023-09-02 19:14:11 +0300

    detect chages in s3 files

--- terragrunt/modules/s3/s3.tf
+++ terragrunt/modules/s3/s3.tf
@@ -60,6 +60,6 @@ resource "aws_s3_object" "source_code" {
   bucket       = aws_s3_bucket.website_bucket.id
   key          = each.value
   source       = "${var.src_path}${each.value}"
-  etag         = filemd5("${var.src_path}${each.value}")
+  etag         = "${filemd5("${var.src_path}${each.value}")}"
   content_type = lookup(local.mime_types, regex("\\.(?P<extension>[A-Za-z0-9]+)$", each.value).extension, "application/octet-stream")
 }
